84db6111dc12d70f208b744d7f7321c6785ce47c
Qurbonov-AA <45850658+Qurbonov-AA@users.noreply.github.com>
2021-04-12 16:21:05 +0500

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -3,14 +3,40 @@ import telebot
 from pprint import pprint
 import nltk
 from random import choice
+import json
 
 # tokenni ruyxatdan utkazish
-bot = telebot.TeleBot("1791602674:AAE5fFNB9-ees55kiCZdtWZxRab3NSrEFH0", parse_mode=None)
-
+bot = telebot.TeleBot("1748606537:AAHwPzN-WCc7R0tn_0LpIiU00XnlSMlwfCU", parse_mode=None)
+BOT_CONFIG = {}
 find = ''
 gintents = ''
 gexamples = ''
 ganswers = ''
+
+# file load from json
+
+def file_open(arg):
+    if (arg == 'uz'):
+        with open('uz.json','r') as f:
+            global BOT_CONFIG
+            BOT_CONFIG = json.load(f)
+            print(BOT_CONFIG['intents'])
+            print(BOT_CONFIG.keys())
+    else:
+        with open('ru.json','r') as f:
+            BOT_CONFIG = json.load(f)
+            print(BOT_CONFIG)
+
+# function for file upload and create
+
+def file_save(arg):
+    if (arg == 'uz'):
+        with open('uz.json','w') as f:
+            json.dump(BOT_CONFIG,f)
+    else:
+        with open('ru.json','w') as f:
+            json.dump(BOT_CONFIG,f)
+
 
 def filter(text):  #filter funksiyasi
     text = text.lower()
@@ -19,24 +45,25 @@ def filter(text):  #filter funksiyasi
     return text
 
 def get_answer_by_intent(intent): # javob izlash funksiyasi
+    global BOT_CONFIG
     answer = BOT_CONFIG["intents"][intent]["answers"]
     return choice(answer)
 
 def add_answers(message):
     chat_id = message.chat.id
-    answers = (message.text).split()
+    answers = (message.text).split(',')
     global BOT_CONFIG
     global gexamples
     global ganswers
     ganswers = answers
     BOT_CONFIG["intents"][gintents]= '{ "examples" : ['+",".join(gexamples)+' ], "answers : [ '+",".join(answers)+' ]"}' 
-    pprint(BOT_CONFIG)
+    file_save('uz')
     text = 'Yangi intent muvofaqiyatli kiritildi sizni tabrikliyman!'
     bot.send_message(message.chat.id,text)
 
 def add_examples(message):
     chat_id = message.chat.id
-    examples = (message.text).split()
+    examples = (message.text).split(',')
     global BOT_CONFIG
     global gexamples
     gexamples = examples
@@ -59,39 +86,11 @@ def add_intents(message):
 # biz hozir start comandasi va help comandasini qaytaishlaymiz
 
 
-BOT_CONFIG ={
-        "intents" :
-           {
-               "hello_men" : {
-                       "examples" : ["Salom","sava","qonday","buloptimi","tinchmi","mazang qalay","qanneysan","qanaqasan","🙋‍♂️"] ,
-                       "answers"  : ["Salomaleykum","Va aleykum as-salom","qandaysan","qalaysan"],},
-               "hello_women":{
-                       "examples" : ["Assalom","qalisan","qalisiz","🙋‍♀️","Assalomu aleykum"],
-                       "answer"  : ["Xayrli kun","🙋‍♀️","ishlariz joyidami"],  },
-               "bye_men"   : {
-                        "examples" : ["bye","bupti","kurishamiz","gaplashamiz"],
-                        "answers"  : ["kurishguncha","xayr","kechroq gaplashamiz"],
-                   },
-               "bye_women"   : {
-                        "examples" : ["paka","xayr","sog buling","xayrcha"],
-                        "answers"  : ["gaplashguncha 🙋‍♀️","xayrcha 😘","yana yozishamiz deb uyliymay 🌹 "], },
-               "qitmirlik"  :{
-                        "examples" :["raqamingdi ber","budilnika quyaman"],
-                        "answers" : ["nima qilasan", "sur","damini ol","toshingni ter","nomerim yuq"],
-                    },
-               "maqasad"    :{
-                        "examples" :["tanishaylik","hikoya ayt","yordam kerak","yordam ber","rasmchiz","yoz","qushiq ayt"],
-                        "answers"  :["yoq","xop","uzur bajarolmayman","gap bulishi mumkinmas","xop demasam uyat bular;)"],
-                   },
-           },
-        "failer_phrases" : ["tushunarli qilib yozing inson ","man bot man xolos tushunarli yozing","nima demoqchiligingizni tushunmadim","balkim uziz javob berarsiz"], }
-
 @bot.message_handler(commands=['start','help','intents'])
 def handle_start_help(message):
+    file_open('uz')
     if(message.text == '/start'):
-        print("foydalanuvchi starni bosdi")
-        bot.reply_to(message, "qonday ishlar ")
-   
+        bot.reply_to(message, "Suhbatlashamizmi???")    
     elif(message.text == '/help'):
         print("helpni bosdi")
         bot.reply_to(message, "qonday yordam kerak ")
@@ -103,6 +102,7 @@ def handle_start_help(message):
 @bot.message_handler(content_types=['document'])
 
 def handle_text_doc(message):
+    file_open('uz')
     bot.reply_to(message, "Men telegram bot man va fayllar bilan ishlay olmayman!?")    
 # ikkinchi dars python da textlar va spiskalar bilan ishlash
 # 
@@ -111,9 +111,10 @@ def handle_text_doc(message):
 def get_intent(message):
     text = message.text
     text = filter(text)
+    global BOT_CONFIG
     global find
-    for intent, data in BOT_CONFIG["intents"].items():
-        for example in data["examples"]:
+    for intent, value in BOT_CONFIG["intents"].items():
+        for example in value["examples"]:
             example = filter(example)
             if (len(example) != 0):
                 distance = nltk.edit_distance(text,example)/len(example)           
